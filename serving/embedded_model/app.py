b14bd282f464ae62d3f11023d6417770506a35ed
Pramod-Kumar808 <pramod-kumar.nagaraj@epita.fr>
2021-06-19 21:13:26 +0200

    result without error

--- serving/embedded_model/app.py
+++ serving/embedded_model/app.py
@@ -6,61 +6,94 @@ $ streamlit run serving/embedded_model/app.py
 
 import streamlit as st
 import requests
+from requests.api import request
 import json
 import joblib
-import numpy as np
+import pandas as pd
+from PIL import Image
 
-model = joblib.load('C:/Users/npram/Desktop/Github/machine-learning-deployment-diabetes/model/diabetes_model.pkl')
+model = joblib.load('C:/Users/npram/Desktop/EPITA/CLASS and NOTES/SEM2/Data_science_production/dsp-correction/models/diabetes_model.pkl')
 
+st.header("Diabetes Prediction for multiple patient")
+st.subheader("Please entre the inputs for Prediction....!")
+# st.set_page_config(page_icon= "https://image.shutterstock.com/image-photo/overt-diabetes-you-yes-no-260nw-1187847934.jpg",)
+
+st.text("Upload your Patient data for the prediction......:)")
+
+#Uploading the csv file
+file = st.file_uploader("Upload the file")
+if file:
+    st.write("Filename ", file.name)
+    dataframe = pd.read_csv(file, sep="\t")
+    st.write(dataframe)
+
+#User inputs
 def run():
-    st.title("Diabetes prediction")
-    age = st.text_input("Age of patient", "59")
-    if (float(age) <= 0):
-        st.warning("Value must be range [1 to 100]")
+    st.sidebar.title("Diabetes prediction for a single Patient")
+    age = st.sidebar.text_input("Age in years", "59")
+    if (float(age) < 0) or (float(age) > 100):
+        st.sidebar.warning("Your not immortal, Please re-enter")
 
-    sex = st.text_input("Gender", "1")
+    sex = st.sidebar.text_input("Gender", "1")
     if (float(sex) < 0) or (float(sex) > 2):
-        st.warning("Mention 2 for Women and 1 for Men")
+        st.sidebar.warning("Mention 2 for Women and 1 for Men")
 
-    bmi = st.text_input("BMI value", "32.1")
+    bmi = st.sidebar.text_input("BMI value", "32.1")
     if (float(bmi) < 0):
-        st.warning("Please enter the integer values")
+        st.sidebar.warning("Please enter correct values")
 
-    bp = st.text_input("Blood pressure", "101")
+    bp = st.sidebar.text_input("Blood pressure", "101")
     if (float(bp) < 0):
-        st.warning("Please enter the integer values")
+        st.sidebar.warning("Please enter correct values")
 
-    s1 = st.text_input("S1 value", "157")
-    s2 = st.text_input("S2 value", "92.3")
-    s3 = st.text_input("S3 value", "38")
-    s4 = st.text_input("S4 value", "4")
-    s5 = st.text_input("S5 value", "4.852")
-    s6 = st.text_input("S6 value", "87")
-
-    data = {
-        "age" : age,
-        "sex" : sex,
-        "bmi" : bmi,
-        "bp" : bp,
-        "s1" : s1,
-        "s2": s2,
-        "s3": s3,
-        "s4" : s4,
-        "s5" : s5,
-        "s6" : s6
-    }
-
-
+    s1 = st.sidebar.text_input("S1 value", "157")
+    s2 = st.sidebar.text_input("S2 value", "92.3")
+    s3 = st.sidebar.text_input("S3 value", "38")
+    s4 = st.sidebar.text_input("S4 value", "4")
+    s5 = st.sidebar.text_input("S5 value", "4.852")
+    s6 = st.sidebar.text_input("S6 value", "87")
+    Y = st.sidebar.text_input("Y", "52")
+# '''
+#     data = {
+#         "age" : age,
+#         "sex" : sex,
+#         "bmi" : bmi,
+#         "bp" : bp,
+#         "s1" : s1,
+#         "s2": s2,
+#         "s3": s3,
+#         "s4" : s4,
+#         "s5" : s5,
+#         "s6" : s6
+#     }
+# '''
+    host = "http://127.0.0.1:8000"
     if st.button("Predict"):
-        response = requests.post("http://127.0.0.1:8000/Predict", json = data)
-        prediction = response.json()
-        # model_input_data = np.array([age, sex, bmi, bp, s1, s2, s3, s4, s5, s6]).reshape(1, -1)
-        # progression = model.predict(model_input_data)
-        # print(type(progression))
-        # return progression[0]
-        # prediction = model.predict(data)
-        st.success(f"The prediction of the model: {prediction}")
+        # response = requests.post("http://127.0.0.1:8000/Predict", json = data)
+        # prediction = response.json()
+        if file:
+            data = dataframe.to_json(orient = "records")
+            connect = f"{host}/Prediction_file"
+            data = data.lower()
+            predictions = requests.post(connect, data = data, headers={'Content-Type': 'application/json'}) 
+            print(predictions.text)
+            predictions = json.loads(json.loads(predictions.text))
+            print(type(predictions))
+            for i in range(5):
+                for j in predictions:
+                    st.info(f"Patient {i} results: {predictions[j]}" )
+        else:
+            "Please upload the data"
 
+    
+    if st.sidebar.button("Prediction"):
+        st.info("Prediction from the database")
+        # st.sidebar.success(f"Result of Patient: {predictions}")
+        connect = f"{host}/Prediction?age={age}&sex={sex}&bmi={bmi}&bp={bp}&s1={s1}&s2={s2}&s3={s3}&s4={s4}&s5={s5}&s6={s6}"
+        predictions = requests.post(connect)
+        st.info(f"Patient Prediction: {predictions.text}")
+    else:
+        print("Please fill the user inputs...!")
 
 if __name__ == '__main__':
     run()
--- serving/model_as_a_service/main.py
+++ serving/model_as_a_service/main.py
@@ -10,11 +10,14 @@ $ curl -X 'POST' 'http://127.0.0.1:8000/predict_obj' \
 """
 
 
+from textwrap import indent
 import uvicorn
 import joblib
+import json
 from fastapi import FastAPI
-import pandas as pd
 from pydantic import BaseModel
+import numpy as np
+from typing import List
 
 app = FastAPI()
 
@@ -23,9 +26,9 @@ import joblib
 patient = []
 
 
-model = joblib.load('C:/Users/npram/Desktop/Github/machine-learning-deployment-diabetes/model/diabetes_model.pkl')
+model = joblib.load('C:/Users/npram/Desktop/EPITA/CLASS and NOTES/SEM2/Data_science_production/dsp-correction/models/diabetes_model.pkl')
 
-class Data(BaseModel):
+class DiabetesInfo(BaseModel):
     age : int
     sex : float
     bmi : float
@@ -44,15 +47,33 @@ def read_home():
     return {"message" : "Welcome to website"}
 
 
-@app.post("/Predict")
-def predict(data : Data):
-    data_dict = data.dict()
-    data_df = pd.DataFrame.from_dict([data_dict])
-    # print(data_df)
-    prediction = model.predict(data_df)
-    prediction_label = ["Patient is healthy" if result == 0 else "Patient need dialysis" for result in prediction]
-    return prediction_label
+@app.post("/Prediction")
+async def predict_diabetes_progresss(age: float, sex: float, bmi: float,bp: float, s1: float, s2: float, s3:float, s4: float, s5: float, s6: float):
+    print(age, sex, bmi, bp, s1, s2, s3, s4, s5, s6)
+    model_input_data = np.array(
+        [age, sex, bmi, bp, s1, s2, s3, s4, s5, s6]).reshape(1, -1)
+    progression = model.predict(model_input_data)
+    # print(type(progression))
+    print(f"model input {model_input_data}")
+    return progression[0]
 
-if __name__ == '__main__':
-    uvicorn.run("main:api", host="127.0.0.1", port=8000, reload=True)
+@app.post("/Prediction_file")
+async def predict_diabetes_progresss(patients: List[DiabetesInfo]):
+    result_list = {}
+    for i, self in  enumerate(patients):
+        input_in_list = np.array([self.age, self.sex, self.bmi, self.bp, self.s1, self.s2, self.s3, self.s4, self.s5, self.s6])
+        input_in_list_reshape = input_in_list.reshape(1, -1)
+        result_list[str(i)] = float(model.predict(input_in_list_reshape))
+        output = json.dumps(result_list)
+        return output
+# def predict(data : Data):
+#     data_dict = data.dict()
+#     data_df = pd.DataFrame.from_dict([data_dict])
+#     # print(data_df)
+#     prediction = model.predict(data_df)
+#     prediction_label = ["Patient is healthy" if result == 0 else "Patient need dialysis" for result in prediction]
+#     return prediction_label
+
+# if __name__ == '__main__':
+#     uvicorn.run("main:api", host="127.0.0.1", port=5000, reload=True)
 
